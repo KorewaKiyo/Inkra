0fd3cef5d4eb3f3827880926e79746a8e8afd986
Liam <liamchelton@gmail.com>
2023-03-20 16:16:44 +0000

    Allow for local debug
    Allow for local debug, speeds things up a bit

--- inkra.py
+++ inkra.py
@@ -1,20 +1,42 @@
 from PIL import Image, ImageFont, ImageDraw
 from font_fredoka_one import FredokaOne
 import datetime
+import requests
+
 
 from inky.auto import auto
 
-inky_display = auto(verbose=True)
-inky_display.set_border(inky_display.WHITE)
+try:
+    inky_display = auto(verbose=True)
+    inkyEnabled = True
+except RuntimeError:
+    print("Inky not found, assuming debug")
+    inkyEnabled = False
 
-displayWidth = inky_display.width
-displayHeight = inky_display.height
-im = Image.new("P", (displayWidth, displayHeight))
+if inkyEnabled:
+    inky_display.set_border(inky_display.BLACK)
+    displayWidth = inky_display.width
+    displayHeight = inky_display.height
+    RED = inky_display.RED
+    BLACK = inky_display.BLACK
+    WHITE = inky_display.WHITE
+    im = Image.new("P", (displayWidth, displayHeight))
+    print(displayHeight)
+else:
+    displayWidth=250
+    displayHeight=122
+    RED = (255,0,0)
+    BLACK = (255,255,255) # Black is white because PIL generates images with a black bg, sue me.
+    WHITE = (0,0,0)
+    im = Image.new("RGB", (displayWidth, displayHeight))
+    print(displayHeight)
+
 draw = ImageDraw.Draw(im)
 font = ImageFont.truetype(FredokaOne, 18)
 
 
 def battery_icon(im, charge: int, pos: int):
+    print(charge,pos)
     if charge <= 15:
         im.paste(Image.open('assets/battery15.png'), (pos, 0))
     elif charge < 30:
@@ -24,6 +46,7 @@ def battery_icon(im, charge: int, pos: int):
     elif charge < 70:
         im.paste(Image.open('assets/battery70.png'), (pos, 0))
     else:
+        print("Batteries full")
         im.paste(Image.open('assets/battery100.png'), (pos, 0))
 
 
@@ -32,21 +55,24 @@ message = f"battery is {battery}%"
 w, h = font.getsize(message)
 x = (displayWidth / 2) - (w / 2)
 y = (displayHeight / 2) - (h / 2)
-print(x, y)
-draw.text((x, y), message, inky_display.RED, font)
+draw.text((x, y), message, RED, font)
 newtime = datetime.datetime.now().strftime('%H:%M')
-draw.text((5, 0), newtime, inky_display.RED, font)
+draw.text((5, 0), newtime, RED, font)
 timewidth, timeheight = font.getsize(newtime)
 timewidth = timewidth + 10
-draw.line((timewidth, 0, timewidth, displayHeight), fill=inky_display.BLACK, width=2)
-draw.line((displayWidth - timewidth, 0, displayWidth - timewidth, displayHeight), fill=inky_display.BLACK, width=2)
+draw.line((timewidth, 0, timewidth, displayHeight), fill=BLACK, width=3)
+draw.line((displayWidth - timewidth, 0, displayWidth - timewidth, displayHeight), fill=BLACK, width=3)
 print(f"right line is {displayWidth - timewidth}")
 print(f"left line is {timewidth}")
 
 batterypos = (displayWidth - timewidth) - timewidth - 48
 battery_icon(im,battery,batterypos)
-im = im.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.FLIP_TOP_BOTTOM)
 
-inky_display.set_image(im)
-inky_display.show(busy_wait=True)
-print(f"Finished displaying {newtime}")
+
+if inkyEnabled:
+    im = im.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.FLIP_TOP_BOTTOM)
+    inky_display.set_image(im)
+    inky_display.show(busy_wait=True)
+    print(f"Finished displaying {newtime}")
+else:
+    im.show()
