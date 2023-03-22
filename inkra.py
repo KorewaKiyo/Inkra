45fb78911b9f84583585add57a550725492c33ca
Liam <liamchelton@gmail.com>
2023-03-22 14:52:03 +0000

    Add cupra logo, tidy code slightly
    Undoing this mess I have made will take a while.

--- inkra.py
+++ inkra.py
@@ -1,19 +1,23 @@
 from PIL import Image, ImageFont, ImageDraw
 from font_fredoka_one import FredokaOne
 import datetime
+import time
 
 inkyReal = False
 try:
     # noinspection PyUnresolvedReferences
     from inky.auto import auto
+
     inky_display = auto(verbose=True)
     inkyReal = True
+
 except RuntimeError as e:  # Linux
     print("Could not initialise display, if you're debugging you can ignore this, printing exception:")
     print(e)
     from inky.mock import InkyMockPHATSSD1608
 
     inky_display = InkyMockPHATSSD1608("red", h_flip=True, v_flip=True)
+
 except ImportError as e:  # Windows
     print("Missing dependencies,")
     print("If you're on windows you can ignore this,")
@@ -35,22 +39,22 @@ draw = ImageDraw.Draw(im)
 font = ImageFont.truetype(FredokaOne, 18)
 
 
-def battery_icon(img, charge: int, pos: int):
-    print(charge, pos)
+def battery_icon(charge: int):
     if charge <= 15:
-        img.paste(Image.open('assets/battery15.png'), (pos, 0))
+        icon = Image.open('assets/battery15.png')
     elif charge < 30:
-        img.paste(Image.open('assets/battery30.png'), (pos, 0))
+        icon = Image.open('assets/battery30.png')
     elif charge < 50:
-        img.paste(Image.open('assets/battery50.png'), (pos, 0))
+        icon = Image.open('assets/battery50.png')
     elif charge < 70:
-        img.paste(Image.open('assets/battery70.png'), (pos, 0))
+        icon = Image.open('assets/battery70.png')
     else:
-        print("Batteries full")
-        img.paste(Image.open('assets/battery100.png'), (pos, 0))
+        icon = Image.open('assets/battery100.png')
+    return icon
 
 
-battery = 85
+
+battery = 12
 message = f"battery is {battery}%"
 w, h = font.getsize(message)
 x = (displayWidth / 2) - (w / 2)
@@ -64,12 +68,20 @@ draw.line((timewidth, 0, timewidth, displayHeight), fill=BLACK, width=3)
 draw.line((displayWidth - timewidth, 0, displayWidth - timewidth, displayHeight), fill=BLACK, width=3)
 print(f"right line is {displayWidth - timewidth}")
 print(f"left line is {timewidth}")
-
-batterypos = (displayWidth - timewidth) - timewidth - 48
-battery_icon(im, battery, batterypos)
-
+# (displayWidth - timewidth) = 199
+# 199 - timewidth = 148
+# 148 - 48 = 100
+batterypos = ((displayWidth - timewidth) - timewidth - 48, 122 - 24)
+print(timewidth)
+im.paste(battery_icon(battery), batterypos)
+im.paste(Image.open("assets/Cupra-Logo.png"), ((displayWidth - timewidth) - timewidth - 48, 0))
 im = im.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.FLIP_TOP_BOTTOM)
-inky_display.set_image(im)
+
 while not inkyReal:
+    inky_display.set_image(im)
     inky_display.show()
+    time.sleep(1)
+inky_display.set_image(im)
+inky_display.show()
+time.sleep(1)
 print(f"Finished displaying {newtime}")
