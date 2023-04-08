ad34d3abb811ff044f19339432f5401f0ab8c2fd
Liam <liamchelton@gmail.com>
2023-04-08 09:57:42 +0100

    Everything inside a function inside a class
    The configurability of the image generation isn't entirely working but that's next
    
    Signed-off-by: Liam <liamchelton@gmail.com>

--- inkra.py
+++ inkra.py
@@ -6,12 +6,10 @@ import math
 
 
 class Inkra:
-    def __init__(self):
+    def __init__(self, flip=True):
         inky_real = True
         try:
-            # noinspection PyUnresolvedReferences
             from inky.auto import auto
-
             inky_display = auto(verbose=True)
 
         except RuntimeError as e:  # Linux
@@ -28,6 +26,8 @@ class Inkra:
             from inky.mock import InkyMockPHATSSD1608
             inky_real = False
             inky_display = InkyMockPHATSSD1608("red", h_flip=True, v_flip=True)
+        except:
+            raise Exception("Unknown error!")
         inky_display.set_border(inky_display.BLACK)
 
         self.width = inky_display.width
@@ -35,10 +35,15 @@ class Inkra:
         self.display = inky_display
         self.display_simulated = inky_real
         self.image = Image.new("P", (self.width, self.height), self.display.WHITE)
+        self.flip = flip
         self.draw = ImageDraw.Draw(self.image)
         self.font = ImageFont.truetype(FredokaOne, 18)
 
     def battery_icon(self, charge: int):
+
+        if charge < 0:
+            raise AttributeError("Battery can not be less than 0%!")
+
         if charge <= 15:
             icon = Image.open('assets/battery15.png')
         elif charge < 30:
@@ -49,58 +54,67 @@ class Inkra:
             icon = Image.open('assets/battery70.png')
         else:
             icon = Image.open('assets/battery100.png')
-        return icon
+        self.icon = icon
 
-    def generate_image(self, show_time=True, show_logo=True, show_bat_icon=True):
+    def generate_image(self, show_time=True, show_logo=True, show_bat_icon=True, draw_lines=True, charge=0):
+
+        # Reset canvas
+        self.image = None
+        self.image = Image.new("P", (self.width, self.height), self.display.WHITE)
+        self.draw = ImageDraw.Draw(self.image)
+
+        message = f"battery is {charge}%"
+        message_bbox = display.font.getbbox(message)
+        message_width = message_bbox[2]
+        message_height = message_bbox[3]
+
+        message_coords = (
+            (display.width / 2) - (message_width / 2),
+            (display.height / 2) - (message_height / 2)
+        )
+        display.draw.text(message_coords, message, display.display.RED, display.font)
+        now = datetime.datetime.now().strftime('%H:%M')
+        timelength = display.font.getbbox(datetime.datetime.now().strftime('%H:%M'))[2]
         if show_time:
-            now = datetime.datetime.now().strftime('%H:%M')
             self.draw.text((5, 0), now, self.display.RED, self.font)
+            timewidth = math.ceil(timelength) + 10
+
         if show_logo:
-            pass
+            display.image.paste(Image.open("assets/Cupra-Logo.png"), ((display.width - timewidth) - timewidth - 48, 0))
         if show_bat_icon:
-            battery = self.battery_icon()
-            battery.width
+            print(charge)
+            self.battery_icon(charge)
+            bat_width = self.icon.width
+            bat_height = self.icon.height
+            bat_pos = ((display.width - timewidth) - timewidth - bat_width, display.height - bat_height)
+            display.image.paste(self.icon, bat_pos)
+        if draw_lines:
+            display.draw.line((timewidth, 0, timewidth, display.height), fill=display.display.BLACK, width=3)
+            display.draw.line((display.width - timewidth, 0, display.width - timewidth, display.height),
+                              fill=display.display.BLACK,
+                              width=3)
+            print(f"right line is {display.width - timewidth}")
+            print(f"left line is {timewidth}")
+            # (display.width - timewidth) = 199
+            # 199 - timewidth = 148
+            # 148 - 48 = 100,
         return self.image
+
+    def push_image(self):
+        if self.flip:
+            self.image = self.image.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.FLIP_TOP_BOTTOM)
+        self.display.set_image(self.image)
+        self.display.show()
 
 
 display = Inkra()
 
 battery = 12
-message = f"battery is {battery}%"
-message_bbox = display.font.getbbox(message)
-message_width = message_bbox[2]
-message_height = message_bbox[3]
-
-message_coords = (
-    (display.width / 2) - (message_width / 2),
-    (display.height / 2) - (message_height / 2)
-)
-display.draw.text(message_coords, message, display.display.RED, display.font)
-display.generate_image(show_logo=False, show_bat_icon=False)
-timelength = display.font.getlength(datetime.datetime.now().strftime('%H:%M'))
-
-timewidth = math.ceil(timelength) + 10
-display.draw.line((timewidth, 0, timewidth, display.height), fill=display.display.BLACK, width=3)
-display.draw.line((display.width - timewidth, 0, display.width - timewidth, display.height), fill=display.display.BLACK,
-                  width=3)
-print(f"right line is {display.width - timewidth}")
-print(f"left line is {timewidth}")
-# (display.width - timewidth) = 199
-# 199 - timewidth = 148
-# 148 - 48 = 100
-
-bat_icon = display.battery_icon(battery)
-bat_width = bat_icon.width
-bat_height = bat_icon.height
-print(bat_width,bat_height)
-batterypos = ((display.width - timewidth) - timewidth - bat_width, display.height - bat_height)
-display.image.paste(bat_icon, batterypos)
-display.image.paste(Image.open("assets/Cupra-Logo.png"), ((display.width - timewidth) - timewidth - 48, 0))
-im = display.image.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.FLIP_TOP_BOTTOM)
 
 while not display.display_simulated:
-    display.display.set_image(im)
-    display.display.show()
+    battery -= 1
+    display.generate_image(show_logo=True, show_bat_icon=True, show_time=True, charge=battery, draw_lines=True)
+    display.push_image()
     time.sleep(1)
 # display.display.set_image(im)
 # display.display.show()
