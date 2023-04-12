2b291cc95aefb4545c1bdad065a8f0b3dee56b00
Liam <liamchelton@gmail.com>
2023-04-12 16:32:04 +0100

    Tidy up, add file for Cupra connectivity

--- inkra.py
+++ inkra.py
@@ -8,9 +8,7 @@ import math
 
 class Terminal:
     header = '\033[95m'
-    okblue = '\033[94m'
-    okcyan = '\033[96m'
-    okgreen = '\033[92m'
+    good = '\033[32m'
     warning = '\033[93m'
     fail = '\033[91m'
     end_colours = '\033[0m'
@@ -27,7 +25,7 @@ class Terminal:
 
 
 class Inkra:
-    def __init__(self, flip: bool = True, weather_enabled: bool = False, city: str = "Guildford", country: str = "GB"):
+    def __init__(self, options: dict, city: str = "Guildford", country: str = "GB"):
         inky_real = True
         try:
             # noinspection PyUnresolvedReferences
@@ -36,7 +34,9 @@ class Inkra:
 
         except RuntimeError as e:  # Linux
             Terminal.warn(
-                f"Could not initialise display, if you're debugging you can ignore this, \nPrinting exception:\n{e}\n")
+                "Could not initialise display, if you're debugging you can ignore this,\n"
+                "Printing exception:\n"
+                f"{Terminal.underline}{e}\n")
             from inky.mock import InkyMockPHATSSD1608
             inky_real = False
             inky_display = InkyMockPHATSSD1608("red", h_flip=True, v_flip=True)
@@ -44,39 +44,40 @@ class Inkra:
         except ImportError as e:  # Windows
             Terminal.warn("\nMissing dependencies,"
                           "If you're on windows you can ignore this,"
-                          f"printing exception:\n{e}\n")
+                          f"printing exception:\n{Terminal.underline}{e}\n")
             from inky.mock import InkyMockPHATSSD1608
             inky_real = False
             inky_display = InkyMockPHATSSD1608("red", h_flip=True, v_flip=True)
 
         inky_display.set_border(inky_display.BLACK)
+        self.options = options
+        self.flip = options["flip"]
+        self.enable_weather = self.options["show_weather"]
+
         self.width = inky_display.width
         self.height = inky_display.height
         self.display = inky_display
 
         self.image = Image.new("P", (self.width, self.height), self.display.WHITE)
-        self.flip = flip
         self.draw = ImageDraw.Draw(self.image)
         self.font = ImageFont.truetype(font_fredoka_one.font, 18)
         with open("assets/banana.otf", "rb") as font:
             self.font = ImageFont.truetype(font, 14)
 
         self.margin = 10
-        self.lineoffset = 53
+        self.line_offset = 53
 
         self.display_real = inky_real
-        self.enable_weather = weather_enabled
 
+        from modules import cupra
+        cupra = cupra.Cupra()
         if self.enable_weather:
             try:
                 from modules import weather
-                self.weather_city = city
-                self.weather_country = country
                 weather = weather.Weather(city, country)
             except ModuleNotFoundError:
-                Terminal.error("\nOne or more modules required for the weather module missing. "
+                Terminal.error("\nOne or more modules required for the weather module missing.\n"
                                "Disabling module")
-
                 self.enable_weather = False
 
     def __battery_icon(self, charge: int):
@@ -96,6 +97,26 @@ class Inkra:
             icon = Image.open('assets/battery100.png')
         self.icon = icon
 
+    def __draw_time(self):
+        time_now = datetime.datetime.now().strftime('%H:%M')
+        date = datetime.datetime.now().strftime('%d/%m')
+
+        time_size = display.font.getbbox(time_now)
+        date_size = display.font.getbbox(date)
+        date_pos = ((self.width - date_size[2] - 5), 0)
+        date_pos = (9, (time_size[3] + 5))
+        self.draw.text(
+            date_pos,
+            date,
+            self.display.RED,
+            self.font
+        )
+        # 9 is chosen as the x because
+        # lineoffset/2 = 26.5px
+        # length of date or time = 17.5px
+        # 26.5 - 17.5 = 9px putting them in the centre
+        self.draw.text((9, 0), time_now, self.display.RED, self.font)
+
     def generate_image(self, show_time=True, show_logo=True, show_bat_icon=True, draw_lines=True, charge=0):
 
         # Reset canvas
@@ -114,38 +135,27 @@ class Inkra:
         )
         display.draw.text(message_coords, message, display.display.RED, display.font)
 
-        if show_time:
-            time = datetime.datetime.now().strftime('%H:%M')
-            date = datetime.datetime.now().strftime('%d/%m')
-            datesize = display.font.getbbox(date)[2]
-            datepos = ((self.width - datesize - 5), 0)
-            print(datesize)
-            self.draw.text(
-                datepos,
-                date,
-                self.display.RED,
-                self.font
-            )
+        if self.options["show_time"]:
+            self.__draw_time()
 
-            self.draw.text((5, 0), time, self.display.RED, self.font)
-
-        if show_logo:
+        if self.options["show_logo"]:
             logo = Image.open("assets/Cupra-Logo.png")
             logo_pos = (
                 math.ceil((self.width / 2) - (logo.width / 2)),
                 0
             )
             self.image.paste(logo, logo_pos)
-        if show_bat_icon:
+
+        if self.options["show_battery_icon"]:
             self.__battery_icon(charge)
             bat_pos = (
                 math.ceil((self.width / 2) - (self.icon.width / 2)),
                 (self.height - self.icon.height)
             )
             self.image.paste(self.icon, bat_pos)
-        if draw_lines:
-            self.draw.line((self.lineoffset, 0, self.lineoffset, self.height), fill=self.display.BLACK, width=3)
-            self.draw.line((self.width - self.lineoffset, 0, self.width - self.lineoffset, self.height),
+        if self.options["draw_lines"]:
+            self.draw.line((self.line_offset, 0, self.line_offset, self.height), fill=self.display.BLACK, width=3)
+            self.draw.line((self.width - self.line_offset, 0, self.width - self.line_offset, self.height),
                            fill=display.display.BLACK,
                            width=3)
         return self.image
@@ -157,7 +167,16 @@ class Inkra:
         self.display.show()
 
 
-display = Inkra(flip=True, weather_enabled=True)
+display_options = {
+    "show_logo": True,
+    "show_battery_icon": True,
+    "show_time": True,
+    "show_weather": True,
+    "draw_lines": True,
+    "flip": True,
+
+}
+display = Inkra(options=display_options)
 
 battery = 45
 
@@ -167,7 +186,12 @@ while True:
         exit(0)
 
     display.generate_image(show_logo=True, show_bat_icon=True, show_time=True, charge=battery, draw_lines=True)
-    display.push_image()
+
+    try:
+        display.push_image()
+    except RuntimeError:
+        break
+
     if display.display_real:
         time.sleep(30)
     else:
---
+++ modules/cupra.py
@@ -0,0 +1,2 @@
+class Cupra:
+    pass
