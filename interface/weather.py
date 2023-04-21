c23f82b52b5088b71ec95000f67ede591f4ee462
Liam <liamchelton@gmail.com>
2023-04-21 16:03:44 +0100

    Move terminal interface into own file

--- interface/weather.py
+++ interface/weather.py
@@ -1,15 +1,27 @@
 import requests
 import geocoder
 
+from .terminal import Terminal
+
 
 class Weather:
+    @classmethod
     def get_coords(self, location):
-        latlng = geocoder.arcgis(location).latlng
-        return latlng
+        geo_result = geocoder.arcgis(location)
+        if geo_result.ok:
+            Terminal.debug(geo_result)
+            latlng = geo_result.latlng
+            Terminal.debug(latlng)
+            return latlng
+        else:
+            raise RuntimeError
 
     def __init__(self, city, country):
         self.coordinates = self.get_coords(f"{city}{country}")
-        print(self.coordinates)
+        self.endpoint = "https://api.open-meteo.com/v1"
 
     def get_weather(self):
-        pass
+        params = {
+            "latitude": self.coordinates[0],
+            "longitude": self.coordinates[1]
+        }
