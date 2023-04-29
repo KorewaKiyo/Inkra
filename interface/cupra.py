4016209ddf6e8c03f935e24569d59e4360982937
Liam <liamchelton@gmail.com>
2023-04-29 10:58:05 +0100

    Start work on cupra API

--- interface/cupra.py
+++ interface/cupra.py
@@ -1,2 +1,10 @@
+import requests
+
+
 class Cupra:
-    pass
+    """Interface for the MyCupra app backend"""
+
+    def __init__(self):
+        self.endpoint = "https://ola.prod.code.seat.cloud.vwgroup.com/v4"
+        self.ua = 'CUPRAApp%20-%20Store/20230404 CFNetwork/1390 Darwin/22.0.0'
+        self.user_id = 3
