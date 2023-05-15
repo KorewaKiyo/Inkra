253a1e5690a2edc09e7d290d70a4a67a3d2cc46a
Liam <liamchelton@gmail.com>
2023-05-15 12:04:45 +0100

    Add an actual exception and tidy up a bit

--- interface/terminal.py
+++ interface/terminal.py
@@ -1,24 +1,27 @@
 class Terminal:
-    header = '\033[95m'
-    good = '\033[32m'
-    warning = '\033[93m'
-    fail = '\033[91m'
-    blue = '\033[34m'
-    end_colours = '\033[0m'
-    bold = '\033[1m'
-    underline = '\033[4m'
+    header = "\033[95m"
+    good = "\033[32m"
+    warning = "\033[93m"
+    fail = "\033[91m"
+    blue = "\033[34m"
+    end_colours = "\033[0m"
+    bold = "\033[1m"
+    underline = "\033[4m"
+
+    class ConfigError(Exception):
+        """Error in the config.yml file."""
 
     @classmethod
     def warn(cls, message):
-        print(f"\n{cls.warning}{message}{cls.end_colours}")
+        print(f"\n{cls.warning}Warning: {message}{cls.end_colours}")
 
     @classmethod
     def error(cls, message):
-        print(f"\n{cls.fail}{message}{cls.end_colours}")
+        print(f"\n{cls.fail}Non-fatal error: {message}{cls.end_colours}")
 
     @classmethod
     def debug(cls, message):
-        print(f"\n{cls.blue}{message}{cls.end_colours}")
+        print(f"\n{cls.blue}DEBUG: {message}{cls.end_colours}")
 
     @classmethod
     def print(cls, message):
