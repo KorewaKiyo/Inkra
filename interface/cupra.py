80ff92e8e6b3ed41abf86977699cc08c331459b0
Liam <liamchelton@gmail.com>
2023-06-14 15:37:31 +0100

    i give up on authentication
    I cannot understand OIDC, so I will use someone elses work to do all the interfacing with MyCupra :)

--- interface/cupra.py
+++ interface/cupra.py
@@ -5,6 +5,12 @@ import yaml
 # Terminal interface
 from interface.terminal import Terminal
 
+from weconnect_cupra import weconnect_cupra
+from weconnect_cupra.service import Service
+from weconnect_cupra.elements.control_operation import ControlOperation
+from weconnect_cupra.api.cupra.domain import Domain
+from weconnect_cupra.api.cupra.elements.enums import MaximumChargeCurrent, UnlockPlugState
+
 
 class Cupra:
     """Interface for the MyCupra app backend"""
@@ -18,40 +24,16 @@ class Cupra:
         if self.config is None:
             raise Terminal.ConfigError("Could not find Cupra section in config.yml")
 
-        self.endpoint = "https://ola.prod.code.seat.cloud.vwgroup.com/"
-        self.ua = "CUPRAApp%20-%20Store/20230404 CFNetwork/1390 Darwin/22.0.0"
-        self.user_id = self.config.get("User_ID")
+        self.username = self.config.get("Username")
+        self.password = self.config.get("Password")
         self.vin = self.config.get("VIN")
-        self.token = self.config.get("Token")
 
-        self.header = {
-            # I don't know the significance of this, but it doesn't hurt
-            "x-csrf-token": "123456789123456789123456789123456789",
-            "user-agent": "CUPRAApp%20-%20Store/20230404 CFNetwork/1390 Darwin/22.0.0",
-            "user-id": self.user_id,
-            "authorization": self.token,
-        }
-
+        self.cupra = weconnect_cupra.WeConnect(username=self.username, password=self.password,
+                                               updateAfterLogin=True, loginOnInit=True,
+                                               service=Service("MYCUPRA"))
         # Same as weather interface, we'll only cache the status request
         # For SoC, Climate, etc. "
         self.last_response = None
 
-    def get_vehicle_status(self):
-        url = f"{self.endpoint}v4/users/{self.user_id}/vehicles/{self.vin}/mycar"
 
-        # Get response object from cache if exists and not more than 5 minutes old.
-        # Otherwise, make another request and store.
-        if self.last_response is None or time.time() - 300 > self.last_response[1]:
-            response = requests.get(url, headers=self.header)
-        else:
-            response = self.last_response[0]
-
-        if response.status_code != 200:
-            Terminal.error(f"Request failed, reason was: {response.reason}")
-            return None
-
-        self.last_response = (response, time.time())
-        battery_status = response.json().get("engines").get("primary")
-        charge_status = response.json().get("services").get("charging")
-        climate_status = response.json().get("services").get("climatisation")
-        return battery_status, charge_status, climate_status
+        def
--- interface/openid.py
+++ interface/openid.py
@@ -3,14 +3,37 @@ import os
 import binascii
 import hashlib
 from bs4 import BeautifulSoup
+from requests.models import CaseInsensitiveDict
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+
+from oauthlib.oauth import prepare_grant_uri
 
 
 class OpenID:
-    def __init__(self):  # , username, password):
+    def __init__(self, username: str = None, password: str = None):
+        if username is None or password is None:
+            raise
+
         self.session = requests.session()
+        retries = Retry(total=5,
+                        backoff_factor=2,
+                        status_forcelist=[500],
+                        raise_on_status=False)
+        self.session.mount('https://', HTTPAdapter(max_retries=retries))
+        self.session.headers = CaseInsensitiveDict({
+            'content-type': 'application/json',
+            'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+            'accept': '*/*',
+            'accept-language': 'de-de',
+            'accept-encoding': 'gzip, deflate, br'
+        })
 
         self.client_id = "3c756d46-f1ba-4d78-9f9a-cff0d5292d51@apps_vw-dilab_com"
         self.redirect_uri = "cupra://oauth-callback"
+
+        self.username = username
+        self.password = password
 
         # Random hex string for CSRF token
         self.state = binascii.b2a_hex(os.urandom(16)).upper()
@@ -34,17 +57,38 @@ class OpenID:
             "code_challenge": hashlib.sha256(self.state)
         }
 
-        response = requests.get("https://identity.vwgroup.io/oidc/v1/authorize", params)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        auth_response = self.session.get("https://identity.vwgroup.io/oidc/v1/authorize", allow_redirects=False)
+        print(auth_response.status_code)
+        soup = BeautifulSoup(auth_response.text, 'html.parser')
 
         # These values associate the authorization requests together, I believe.
         self.csrf = soup.find(id="csrf").attrs.get("value")
         self.hmac = soup.find(id="hmac").attrs.get("value")
         self.relay_state = soup.find(id="input_relayState").attrs.get("value")
 
-        return response
+        return auth_response
 
-    def signing
+    def signin(self):
+        body = {
+            "_csrf": self.csrf,
+            "relayState": self.relay_state,
+            "hmac": self.hmac,
+            "email": self.username,
 
-cupra = OpenID()
-response = cupra.begin_auth()
+        }
+        loginHeaders = self.session.headers.copy()
+        loginHeaders[
+            "referer"] = f"https://identity.vwgroup.io/signin-service/v1/signin/3c756d46-f1ba-4d78-9f9a-cff0d5292d51" \
+                         f"@apps_vw-dilab_com?relayState={self.relay_state}"
+        loginHeaders["Content-Type"] = 'application/x-www-form-urlencoded'
+
+        signin_response = self.session.post(
+            f"https://identity.vwgroup.io/signin-service/v1/3c756d46-f1ba-4d78-9f9a-cff0d5292d51@apps_vw-dilab_com/login/identifier",
+            data=body,
+            headers=loginHeaders, allow_redirects=True)
+        return signin_response
+
+
+cupra = OpenID("USERNAME@gmail.com", "PASSWORD")
+auth_begin = cupra.begin_auth()
+signin = cupra.signin()
