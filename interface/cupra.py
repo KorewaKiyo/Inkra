ca8c87e59454bb1a2f3179c5a15593007ec6f44a
Liam <liamchelton@gmail.com>
2023-06-15 12:09:01 +0100

    Start adding working functions

--- interface/cupra.py
+++ interface/cupra.py
@@ -1,15 +1,9 @@
-import time
-import requests
 import yaml
+from weconnect_cupra import weconnect_cupra
+from weconnect_cupra.service import Service
 
 # Terminal interface
 from interface.terminal import Terminal
-
-from weconnect_cupra import weconnect_cupra
-from weconnect_cupra.service import Service
-from weconnect_cupra.elements.control_operation import ControlOperation
-from weconnect_cupra.api.cupra.domain import Domain
-from weconnect_cupra.api.cupra.elements.enums import MaximumChargeCurrent, UnlockPlugState
 
 
 class Cupra:
@@ -28,12 +22,34 @@ class Cupra:
         self.password = self.config.get("Password")
         self.vin = self.config.get("VIN")
 
-        self.cupra = weconnect_cupra.WeConnect(username=self.username, password=self.password,
-                                               updateAfterLogin=True, loginOnInit=True,
-                                               service=Service("MYCUPRA"))
+        self.cupra_api = weconnect_cupra.WeConnect(
+            username=self.username,
+            password=self.password,
+            maxAge=300,
+            tokenfile="token.txt",
+            updateAfterLogin=True,
+            loginOnInit=True,
+            service=Service("MyCupra"),
+        )
+
+        self.cupra_api.persistTokens()
+
+        # If the VIN hasn't been defined, but login succeeded,
+        # We can grab the first vehicle's VIN from the account and use that
+        if self.vin is None:
+            self.vin = str(self.cupra_api.vehicles.children[0].vin)
+
         # Same as weather interface, we'll only cache the status request
         # For SoC, Climate, etc. "
         self.last_response = None
 
-
-        def
+    def get_battery_status(self):
+        status = (
+            self.cupra_api.vehicles.get(self.vin)
+            .domains.get("charging", [])
+            .get("batteryStatus")
+        )
+        if status is not None:
+            return status
+        else:
+            raise
